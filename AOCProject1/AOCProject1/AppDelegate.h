c62a0e90e392c306fbdbb48e42707d098014fb9c
Mark McAninch <awacs68@fullsail.udu>
2013-05-08 23:26:00 -0400

    Created Project 1 first commit

---
+++ AOCProject1/AOCProject1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AOCProject1
+//
+//  Created by Mark A McAninch on 5/8/13.
+//  Copyright (c) 2013 Mark A McAninch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AOCProject1/AOCProject1/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  AOCProject1
+//
+//  Created by Mark A McAninch on 5/8/13.
+//  Copyright (c) 2013 Mark A McAninch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
