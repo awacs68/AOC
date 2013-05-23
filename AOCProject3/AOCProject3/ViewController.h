4e5395b8cb36a8afd0e242c7d31a883aeaaf020b
Mark McAninch <awacs68@fullsail.udu>
2013-05-23 19:00:54 -0400

    Starting Project 3

---
+++ AOCProject3/AOCProject3/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AOCProject3
+//
+//  Created by Mark A McAninch on 5/23/13.
+//  Copyright (c) 2013 Mark A McAninch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ AOCProject3/AOCProject3/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  AOCProject3
+//
+//  Created by Mark A McAninch on 5/23/13.
+//  Copyright (c) 2013 Mark A McAninch. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    
+// Start Project 3
+    
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
