b47ad66216e333320f2df8e0d97d2a43007137de
Mark McAninch <awacs68@fullsail.udu>
2013-05-29 23:37:22 -0400

    Created all buttons and text fieldsand info function working

--- AOCProject4/AOCProject4/ViewController.h
+++ AOCProject4/AOCProject4/ViewController.h
@@ -9,5 +9,8 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController <UIAlertViewDelegate>
-
+{
+    UILabel *infoLabel;
+}
+-(void)onClick:(UIButton*)button;
 @end
--- AOCProject4/AOCProject4/ViewController.m
+++ AOCProject4/AOCProject4/ViewController.m
@@ -8,6 +8,9 @@
 
 #import "ViewController.h"
 
+#define BUTTON_ZERO 0
+#define DATEBUTTON_ONE 1
+#define INFOBUTTON_TWO 2
 @interface ViewController ()
 
 @end
@@ -17,35 +20,105 @@
 - (void)viewDidLoad
 {
 // Background Color
-  //  self.view.backgroundColor = [UIColor whiteColor];
-// UserName 
+    self.view.backgroundColor = [UIColor whiteColor];
+// User Name Label
     UILabel *userLabel;
-    userLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 32, 90, 30)];
+    userLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 15, 90, 30)];
     if (userLabel != nil)
     {
         userLabel.text = @"Username:";
         [self.view addSubview:userLabel];
     }
-    UITextField *textField = [[UITextField alloc] initWithFrame:CGRectMake(90.0f, 32.0f, 220.0f, 30.0f)];
+    // User Name Textfield
+    UITextField *textField = [[UITextField alloc] initWithFrame:CGRectMake(90.0f, 15.0f, 220.0f, 30.0f)];
     if (textField != nil)
     {
-        textField.borderStyle = UIButtonTypeRoundedRect;
+        textField.borderStyle = UITextBorderStyleRoundedRect;
         [self.view addSubview:textField];
     }
-    
+    // Login Button
     UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
     if (button != nil)
     {
-        button.frame = CGRectMake(210.0f, 80.0f, 100.0f, 40.0f);
-        [button setTitle:@"Login" forState:UIControlStateNormal];
-        [button addTarget:self action:@selector(onClick) forControlEvents:UIControlEventTouchUpInside];
+        button.frame = CGRectMake(210.0f, 55.0f, 100.0f, 40.0f);
+        button.tintColor = [UIColor orangeColor];
+        [button addTarget:self action:@selector(onClick:) forControlEvents:UIControlEventTouchUpInside];        [button setTitle:@"Login" forState:UIControlStateNormal];
+        button.tag = BUTTON_ZERO;
         [self.view addSubview:button];
+    }
+    // User Name Display
+    UILabel *nameLabel;
+    nameLabel = [[UILabel alloc] initWithFrame:CGRectMake(60, 110, 320, 65)];
+    if (nameLabel != nil)
+    {
+        nameLabel.textColor = [UIColor blueColor];
+        nameLabel.text = @"Please Enter Username";
+        [self.view addSubview:nameLabel];
+    }
+    // Date Button
+    UIButton *dateButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    if (dateButton != nil)
+    {
+        dateButton.frame = CGRectMake(10.0f, 240.0f, 100.0f, 40.0f);
+        dateButton.tintColor = [UIColor orangeColor];
+        [dateButton addTarget:self action:@selector(onClick:) forControlEvents:UIControlEventTouchUpInside];
+        [dateButton setTitle:@"Show Date" forState:UIControlStateNormal];
+        dateButton.tag = DATEBUTTON_ONE;
+        [self.view addSubview:dateButton];
+    }
+    // Information Button
+    UIButton *infoButton = [UIButton buttonWithType:UIButtonTypeInfoDark];
+    if (infoButton != nil)
+    {
+        infoButton.frame = CGRectMake(10.0f, 340.0f, 25.0f, 25.0f);
+        infoButton.tag = INFOBUTTON_TWO;
+        [infoButton addTarget:self action:@selector(onClick:) forControlEvents:UIControlEventTouchUpInside];
+        [self.view addSubview:infoButton];
+    }
+    // Info Label
+    infoLabel = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 380.0f, 320.0f, 75.0f)];
+    if (infoLabel != nil)
+    {
+        infoLabel.textColor = [UIColor blueColor];
+        infoLabel.numberOfLines = 2;
+    
+    }
+    // Date
+    NSDate *date = [NSDate date];
+    
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    if (dateFormatter != nil)
+    {
+        [dateFormatter setDateStyle:NSDateFormatterMediumStyle];
+        [dateFormatter setTimeStyle:NSDateFormatterFullStyle];
+        
     }
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
--(void)onClick
+    // On Click
+-(void)onClick:(UIButton*)button
 {
+    if (button.tag == BUTTON_ZERO)
+    {
+        
+    }
+    else if (button.tag == DATEBUTTON_ONE)
+    {
+     UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Username!" message:button.titleLabel.text  delegate:nil cancelButtonTitle:@"You are logged in!" otherButtonTitles:nil];
+    if (alertView != nil)
+        {
+            [alertView show];
+        }
+    }
+    else if (button.tag == INFOBUTTON_TWO)
+    {
+        infoLabel.text = @"This application was created by: Mark McAninch";
+        infoLabel.backgroundColor = [UIColor whiteColor];
+        [self.view addSubview:infoLabel];
+       // NSLog(@"It worked");
+    }
+   
     
 }
 
